776f29a3dc49a41fdfc81829deb53c4304e9492c
Yesi Gebreselassie <kalisecurity@yandex.com>
2026-05-14 08:30:18 +0000

    Add script to configure Application Event Log size
    This script configures the maximum size of the Windows Application Event Log to 32 MB by creating or updating the corresponding registry value.

---
+++ STIGS/WN11-AU-000500.ps1
@@ -0,0 +1,56 @@
+
+<#
+.SYNOPSIS
+    Toggles cryptographic protocols (secure vs insecure) on the system.
+    Please test thoroughly in a non-production environment before deploying widely.
+    Make sure to run as Administrator or with appropriate privileges.
+
+.NOTES
+    Author          : Yesi Gebreselassie
+    LinkedIn        : inkedin.com/in/yesi-gebreselassie
+    GitHub          : github.com/hackeryesi
+     Website	   : thethirdeyehackers.com
+    Date Created    : 2026-05-13
+    Last Modified   : 2026-05-13
+    Version         : 1.0
+    CVEs            : CVE-2014-3566
+                      CVE-2021-23839
+                      CVE-2011-3389
+    Plugin IDs      : 20007
+                      104743
+                      157288
+    STIG-ID         : WN11-AU-000500
+
+.TESTED ON
+    Date(s) Tested  : 2026-05-13
+    Tested By       : Yesi Gebreselassie
+    Systems Tested  : Windows Enterprise  Build 29H2
+    PowerShell Ver. : 5.1.17763.6189
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\WN11-AU-000500.ps1 
+#>
+
+# Configure Windows Application Event Log Maximum Size to 32 MB
+# STIG: WN11-CC-000XXX (Example hardening setting)
+
+$RegistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application"
+$ValueName = "MaxSize"
+$ValueData = 0x00008000   # 32768 KB = 32 MB
+
+# Create registry path if it does not exist
+if (-not (Test-Path $RegistryPath)) {
+    New-Item -Path $RegistryPath -Force | Out-Null
+}
+
+# Create or update the registry value
+New-ItemProperty `
+    -Path $RegistryPath `
+    -Name $ValueName `
+    -Value $ValueData `
+    -PropertyType DWord `
+    -Force | Out-Null
+
+Write-Host "Application Event Log maximum size configured successfully."
